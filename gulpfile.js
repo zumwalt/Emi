d3c0d12c72222bad0feac4be31e97aeebb952f22
Casey Zumwalt <casey@simplefocus.com>
2014-09-16 12:55:17 -0700

    Initial changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    sass = require('gulp-ruby-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    newer = require('gulp-newer'),
-    imagemin = require('gulp-imagemin'),
-    git = require('gulp-git'),
-    livereload = require('gulp-livereload'),
-    lr = require('tiny-lr'),
-    server = lr();
+var gulp          = require('gulp'),
+    browserSync   = require('browser-snyc'),
+    reload        = browserSync.reload,
+    plumber       = require('gulp-plumber'),
+    sass          = require('gulp-ruby-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    minifycss     = require('gulp-minify-css'),
+    newer         = require('gulp-newer'),
+    imagemin      = require('gulp-imagemin'),
+    git           = require('gulp-git');
 
+var host = "yourlocal.dev";
 var imgSrc = 'assets/images/originals/*';
 var imgDest = 'assets/images';
-    
+
 gulp.task('styles', function(){
   return gulp.src(['scss/*.scss',
-                   'scss/**/*.scss'], 
+                   'scss/**/*.scss'],
             {base: 'scss/'} )
       .pipe(plumber())
 	    .pipe(sass({ style: 'expanded' }))
@@ -23,6 +23,7 @@ gulp.task('styles', function(){
 	    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
 	    .pipe(minifycss())
 	    .pipe(gulp.dest(''));
+      .pipe(reload({stream:true}));
 });
 
 gulp.task('images', function() {
@@ -32,29 +33,26 @@ gulp.task('images', function() {
         .pipe(gulp.dest(imgDest));
 });
 
-gulp.task('default', ['styles', 'images']);
-
-gulp.task('watch', function() {
-  // Listen on port 35729
-  server.listen(35729, function (err) {
-      if (err) {
-        return console.log(err);
-      }
-  
-      // Watch .scss files
-      gulp.watch('scss/*.scss', ['styles']);
-      gulp.watch('scss/**/*.scss', ['styles']);
-      gulp.watch('assets/images/originals/**', ['images']);
-  
+gulp.task('browser-sync', function() {
+    browserSync({
+        proxy: host
     });
-
 });
+
+gulp.task('default', ['styles', 'images', 'browser-sync'], function () {
+  gulp.watch('scss/*.scss', ['styles']);
+  gulp.watch('scss/**/*.scss', ['styles']);
+  gulp.watch('assets/images/originals/**', ['images']);
+});
+
 gulp.task('init', function(){
 	git.init();
 });
+
 gulp.task('commit', function(){
   return gulp.src('./*')
   .pipe(git.add())
   .pipe(git.commit('initial commit'));
 });
+
 gulp.task('setup',['styles','init','commit']);
